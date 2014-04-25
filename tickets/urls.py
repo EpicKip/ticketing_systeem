7b2690c8cec9428417cf747f6b446bb7d910b99d
EpicKip <kippppp44@gmail.com>
2014-04-25 17:59:12 +0200

    COMMIT! :D:D:D:D

--- tickets/urls.py
+++ tickets/urls.py
@@ -4,11 +4,11 @@ from django.conf.urls import url
 from tickets.models import Event
 
 urlpatterns = [
-    url('^$', 'tickets.views.index', name='indexS'),
+    url('^$', 'tickets.views.index', name='index'),
     url(r'(?P<event_id>\d+)/$', 'tickets.views.show_event', name='show_event'),
     url(r'(?P<event_id>\d+)/step2/$', 'tickets.views.step2', name='step2'),
     url(r'(?P<event_id>\d+)/step3/$', 'tickets.views.step3', name='step3'),
     url(r'(?P<event_id>\d+)/step4/$', 'tickets.views.step4', name='step4'),
-    url(r'^$', 'tickets.views.login', name='login'),
-    url(r'^$', 'django.contrib.auth.views.logout', name='logout'),
+    url(r'^login/$', 'tickets.views.user_login', name='login'),
+    url(r'^logout/$', 'tickets.views.user_logout', name='logout'),
 ]
--- tickets/views.py
+++ tickets/views.py
@@ -1,4 +1,7 @@
-from django.contrib.auth import authenticate, login
+from datetime import datetime, date
+from django.contrib.auth import authenticate, login, logout
+from django.core.urlresolvers import reverse
+from django.http import HttpResponseRedirect, HttpResponse
 from django.shortcuts import render, render_to_response
 from django.template import RequestContext
 from tickets.models import Event
@@ -9,15 +12,50 @@ def index(request):
     return render_to_response('base.html', {'events': events}, context_instance=RequestContext(request))
 
 
-def login(request):
-    username = request.POST['username']
-    password = request.POST['password']
-    user = authenticate(username=username, password=password)
-    if user is not None:
-        login(request, user)
+def user_login(request):
+    # Like before, obtain the context for the user's request.
+    context = RequestContext(request)
+
+    # If the request is a HTTP POST, try to pull out the relevant information.
+    if request.method == 'POST':
+        # Gather the username and password provided by the user.
+        # This information is obtained from the login form.
+        username = request.POST['username']
+        password = request.POST['password']
+
+        # Use Django's machinery to attempt to see if the username/password
+        # combination is valid - a User object is returned if it is.
+        user = authenticate(username=username, password=password)
+
+        # If we have a User object, the details are correct.
+        # If None (Python's way of representing the absence of a value), no user
+        # with matching credentials was found.
+        if user is not None:
+            # Is the account active? It could have been disabled.
+            if user.is_active:
+                # If the account is valid and active, we can log the user in.
+                # We'll send the user back to the homepage.
+                login(request, user)
+                return HttpResponse("Logged in! Just press back for the time being.. Im gonna fix it soon")
+            else:
+                # An inactive account was used - no logging in!
+                return HttpResponse("Your ticketing account is disabled.")
+        else:
+            # Bad login details were provided. So we can't log the user in.
+            print "Invalid login details: {0}, {1}".format(username, password)
+            return HttpResponse("Invalid login details supplied.")
+
+    # The request is not a HTTP POST, so display the login form.
+    # This scenario would most likely be a HTTP GET.
     else:
-        # Return an 'invalid login' error message.
+        # No context variables to pass to the template system, hence the
+        # blank dictionary object...
         pass
+
+
+def user_logout(request):
+    logout(request)
+    return HttpResponse("Logged out! Just press back for the time being.. Im gonna fix it soon")
 
 
 def show_event(request, event_id):
