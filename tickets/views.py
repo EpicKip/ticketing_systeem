99488bfe243f9c1824ecf38e1eb9bb76c3aaa2ea
EpicKip <kippppp44@gmail.com>
2014-06-20 17:29:28 +0200

    Hopelijk laatste commit

--- tickets/views.py
+++ tickets/views.py
@@ -4,7 +4,7 @@ from django.core.mail import send_mail
 
 __author__ = 'Aaron'
 from django.core.urlresolvers import reverse
-from django.http import HttpResponseRedirect, HttpResponse
+from django.http import HttpResponseRedirect
 from django.contrib.auth import authenticate, login, logout
 from django.shortcuts import render_to_response
 from django.template import RequestContext
